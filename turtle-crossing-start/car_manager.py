fd7637e9fef51ff231cca98c2a0d2183bda6cdb5
Henis Patel <henis.patel@ucalgary.ca>
2026-06-01 10:47:38 -0600

    finished the turtle crossing the road capstone project

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -1,7 +1,31 @@
+import random
+from turtle import Turtle
+
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager: 
+    def __init__(self):
+        self.move_distance = STARTING_MOVE_DISTANCE
+        self.cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.goto(300, random.randint(-250, 250))
+            self.cars.append(new_car)
+    
+    def move_cars(self):
+        for car in self.cars:
+            car.backward(self.move_distance)
+    
+    def increase_speed(self):
+        self.move_distance += MOVE_INCREMENT
--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,7 +8,29 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkeypress(player.move, "Up")
+screen.onkeypress(player.down, "Down")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+    
+    # Detect collision with car
+    for car in car_manager.cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+    
+    # Detect successful crossing
+    if player.is_at_finish_line():
+        player.reset_position()
+        car_manager.increase_speed()
+        scoreboard.increase_score()
